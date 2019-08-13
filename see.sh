6d2f00648d1ac007e8fa87269712adfd0f1e7b5a
Richard Jarram <rjarram@me.com>
2019-08-13 17:11:19 -0400

    finalised script

--- see.sh
+++ see.sh
@@ -1,28 +1,24 @@
+# Get name of project frm user input at script execution.
+project_name=${1?Error: No project name given}
 
-# Get name of project and store it in variable
-echo "Please enter a project name."
-read project_name
-echo "You entered '$project_name'."
-
-# Search through the codebase and fetch all matching paths
+# Change directory to source code directory.
 eval "cd ~/code/catonmat"
+
+# Search through the codebase and fetch all matching paths.
 # Change delimiter from \n to : to make it easier to handle.
 path_list=$(eval "ls | grep $project_name | tr '\n' ':' ")
 
 # DEBUG: Examine output of output variable.
-declare -p path_list
+# declare -p path_list
 
-# Set Sublime Text open binary path
+# Set Sublime Text binary path.
 stt="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 
-# Get the first element of the path set
+# Get the first element of the path set.
 path="$(cut -d ':' -f1 <<< "$path_list")"
 
-# open STT in the path directory
+# open STT in the path directory.
 eval "${stt} ~/code/catonmat/${path}"
 
-# open new Terminal window at directory
+# open new Terminal window at directory.
 eval "/usr/bin/open -a Terminal '${path}'"
-
-
-
---
+++ test.sh
@@ -0,0 +1,3 @@
+name1=${1?Error: no name given}
+name2=${2:-friend}
+echo "Hello ${name1} ${name2}!"
